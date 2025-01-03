a7448acdb2bcf8eb8586801a98e6666935f81723
Taffarel Campos <taffarel_lc@hotmail.com>
2025-01-02 23:08:56 -0300

    Criando layout inicial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
+    implementation(libs.circleimageview)
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
