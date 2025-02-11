b20d403fae888e4982e5bdcb46e7ebb418439507
Taffarel Campos <taffarel_lc@hotmail.com>
2025-02-10 23:52:18 -0300

    Implementado busca de dados do usuário

--- app/src/main/java/com/example/agenda/FireUserDataSorce.kt
+++ app/src/main/java/com/example/agenda/FireUserDataSorce.kt
@@ -1,4 +1,21 @@
 package com.example.agenda
 
+import com.example.agenda.common.model.User
+import com.google.firebase.firestore.FirebaseFirestore
+
 class FireUserDataSource : UserDataSource {
+    override fun fetchUser(userUUID: String, callback: UserCallback<Pair<User, Boolean?>>) {
+        FirebaseFirestore.getInstance()
+            .collection("/users")
+            .document(userUUID)
+            .get()
+            .addOnSuccessListener { document ->
+                val user = document.toObject(User::class.java)
+                callback.onSuccess(Pair(user, null))
+            }
+            .addOnFailureListener { exception ->
+                callback.onFailure(exception.message ?: "Erro ao buscar usuário")
+            }
+
+    }
 }
--- app/src/main/java/com/example/agenda/UserDataSource.kt
+++ app/src/main/java/com/example/agenda/UserDataSource.kt
@@ -1,4 +1,8 @@
 package com.example.agenda
 
+import com.example.agenda.common.model.User
+
 interface UserDataSource {
+
+    fun fetchUser(userUUID: String, callback: UserCallback<Pair<User, Boolean?>>)
 }
